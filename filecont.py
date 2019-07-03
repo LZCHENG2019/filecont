fcfef96cb0bd1f3bd9731878129b938d7e52270d
LZCHENG2019 <46548306+LZCHENG2019@users.noreply.github.com>
2019-07-03 11:10:20 +0800

    0703;提交给杰哥前比较完美的一篇

--- filecont.py
+++ filecont.py
@@ -9,18 +9,20 @@ def un_compress(file):#识别压缩包类型并解压，可能不用
     if os.path.exists(file):
         kind = fileguess(file)#filetype.guess(file)
         path = os.path.split(file)[0]#获取文件路径，例：/root/demo
-        fname = os.path.splitext(file)[0]#解压后的文件路径
-        if kind is None:
-            print('未知的文件类型！！')
-            return
+        if kind == 'XZ compressed data':
+            dirnm = subprocess.getstatusoutput("tar Jxvf %s -C %s | xargs awk 'BEGIN{print ARGV[1]}'" % (file, path))
+            print('%s 解压完成' %file)
+        elif kind == 'POSIX tar archive (GNU)':
+            dirnm = subprocess.getstatusoutput("tar xvf %s -C %s | xargs awk 'BEGIN{print ARGV[1]}'" %(file,path))
+            print('%s 解压完成' % file)
+        elif kind == 'gzip compressed data':
+            dirnm = subprocess.getstatusoutput("tar zxvf %s -C %s | xargs awk 'BEGIN{print ARGV[1]}'" % (file, path))
+            print('%s 解压完成' % file)
         else:
-            un_tar(file,path)
-            str1 = fname.split('.')
-            if str1 is None:
-                return
-            else:
-                fname = str1[0]
-            return fname
+            print('未知文件类型')
+
+        dirname = path + '/'+dirnm[1].split('/')[0]
+        return dirname
 
 def fileguess(file):#获取文件类型
     if os.path.exists(file):
@@ -30,24 +32,9 @@ def fileguess(file):#获取文件类型
     else:
         fg = '/'
         return fg
-    #     fg = filetype.guess(file)
-    #     if fg is None:
-    #         fg1 = os.path.splitext(file)[1]
-    #         # print('文件类型：%s' % fg1)
-    #     else:
-    #         fg1 = fg.extension
-    #         # print('文件类型: %s' % fg1)
-    #     return fg1
-    #     #return fg.extension
-    # else:
-    #     fg1 = '/'
-    #     return fg1
 def filesize(file):#获取文件尺寸，以字节（b）为单位
     if os.path.exists(file):
         fs = os.path.getsize(file)
-        return fs
-    else:
-        fs = '/'
         return fs
 
 def getMd5(file):#获取MD5值
@@ -99,13 +86,18 @@ def comparfile(file1,file2):#对比文件
                 return result
         return result
     else:
-        print(' %s 和 %s 类型不同，不继续对比' %(file1,file2))
+        print(' %s 和 %s 类型不同，不进行对比' %(file1,file2))
         result = False
         return result
+
+level = 1
 def compardirs(path1,path2):#对比文件夹内容
     file1m = 0
     file2m = 0
-    diff = 0
+    diff = []
+    diffdirs = 1
+    diffnum = 2
+    print('文件夹%s 和文件夹 %s 对比内容如下：' %(path1,path2))
     finresult = True
     for root, dirs, files in os.walk(path1):
         for name in files:
@@ -113,28 +105,35 @@ def compardirs(path1,path2):#对比文件夹内容
             file2 = os.path.join(path2, name)
             if os.path.exists(file1) and os.path.exists(file2):#如果AB两个文件都存在，则进一步对比
                 result = comparfile(file1,file2)
-                if result:
+                if result:#如果对比结果一致则返回True
                     finresult = True
-                else:
-                    diff += 1
+                else:#
+                    global level
+                    if level ==1:
+                        f1 = fileguess(file1)
+                        f2 = fileguess(file2)
+                        if f1 and f2 in {'XZ compressed data','gzip compressed data'}:
+                            file1path = un_compress(file1)  # filepath是解压后文件所在目录
+                            file2path = un_compress(file2)
+                            compardirs(file1path, file2path)
+                    diff.append(name)
             elif os.path.exists(file1):#任何一个文件不存在则，输出一个数值
                 file1m +=1
                 print('%s 不存在' %file2)
             elif os.path.exists(file2):
                 file2m +=1
                 print('%s 不存在' % file1)
-    if abs(file1m-file2m) > 1:#此阈值是个可调值。用来显示文件夹内文件数量差距过大时报警
+    if abs(file1m-file2m) > diffdirs:#此阈值是个可调值。用来显示文件夹内文件缺少/增多数量差距过大时报警
         print('文件夹内文件数量差距过多')
         finresult = False
         return finresult
-    if diff > 2:#此阈值是个可调值。用来显示文件夹内文件差距过大时报警
+    if len(diff) > diffnum:#此阈值是个可调值。用来显示文件夹内文件不同数据过大时报警
         print('文件夹内差异文件数大于阈值')
         finresult = False
         return finresult
 
 def Main(file1,file2):#,book_name_xls):
     if os.path.exists(file1) and os.path.exists(file2):
-
         size1 = filesize(file1)#文件1的大小
         size2 = filesize(file2)#文件2的大小
         if size1 == size2:     #首先对比文件大小，若文件大小一样，则继续对比MD5值
@@ -147,20 +146,19 @@ def Main(file1,file2):#,book_name_xls):
                 file1path = un_compress(file1)#filepath是解压后文件所在目录
                 file2path = un_compress(file2)
                 if compardirs(file1path,file2path):
-                    print('结果无差异')
+                    print('最终结果：结果无差异')
                 else:
-                    print('差异过大，报警！')
+                    print('最终结果：结果有差异请查看！')
         elif abs(size1-size2) > 10:#此处阈值可调，当文件差异过大时，继续对比
             file1path = un_compress(file1)  # filepath是解压后文件所在目录
             file2path = un_compress(file2)
             if compardirs(file1path, file2path):
                 print('最终结果：结果无差异')
             else:
-                print('最终结果：差异过大，报警！')
+                print('最终结果：结果有差异请查看！')
         else:
             print('最终结果：文件差异在允许范围内')
     else:
         print('文件不存在，请重新输入')
 if __name__=='__main__':
     Main(input('基准文件:'), input('受试文件:'))
-
